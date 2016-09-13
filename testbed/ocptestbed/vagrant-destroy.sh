0287140930da4dbc383f5861c8d5b33a0ada1f74
Javi Roman <jroman.espinar@gmail.com>
2016-09-13 11:15:48 +0200

    workaround to libvirt multidisk issue 651

---
+++ testbed/ocptestbed/vagrant-destroy.sh
@@ -0,0 +1,7 @@
+vagrant destroy mn
+vagrant destroy master
+vagrant destroy node1
+vagrant destroy node2
+vagrant destroy node3
+vagrant destroy node4
+
---
+++ testbed/ocptestbed/vagrant-up.sh
@@ -0,0 +1,6 @@
+vagrant up mn master node1 --no-provision
+vagrant up node2 --no-provision
+vagrant up node3 --no-provision
+vagrant up node4 --no-provision
+vagrant provision 
+
