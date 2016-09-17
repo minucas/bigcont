f7a2a2c15b7715871108b25d7ff9b422cb7dc909
Javi Roman <jroman.espinar@gmail.com>
2016-09-17 10:03:37 +0200

    fixed readme

--- testbed/ocptestbed/vagrant-up.sh
+++
@@ -1,7 +0,0 @@
-vagrant up mn master --no-provision
-vagrant up node1 --no-provision
-vagrant up node2 --no-provision
-vagrant up node3 --no-provision
-vagrant up node4 --no-provision
-vagrant provision 
-
