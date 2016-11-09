c6d66dcdd34ba6ffcf1cd741d3bb93cbe94f2668
Javi Roman <jroman.espinar@gmail.com>
2016-11-09 07:13:28 +0000

    step ahead with zookeeper template

--- zookeeper/template/configmaps/create-configmap.sh
+++ zookeeper/template/configmaps/create-configmap.sh
@@ -1,2 +1,3 @@
-oc create configmap zookeeper-config \
-    --from-file=zoo.cfg
+oc create configmap zk-1-config --from-file=zk-1/zoo.cfg
+oc create configmap zk-2-config --from-file=zk-2/zoo.cfg
+oc create configmap zk-3-config --from-file=zk-3/zoo.cfg
